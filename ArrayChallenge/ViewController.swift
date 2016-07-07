412bb13a8f123e15ae5e0fff01db7dd6c58b0488
Sara <saraahmad1114@yahoo.com>
2016-07-07 14:38:43 -0400

    shopping list

--- ArrayChallenge/ViewController.swift
+++ ArrayChallenge/ViewController.swift
@@ -14,10 +14,26 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        let ingredients = ["Bananas","Apples", "Eggs", "Rolls"]
+        let quantity = [6, 4, 12, 4]
+        shoppingList = makeShoppingList(ingredients, quantityOfItems: quantity)
         
+    }
     
-        
-        
+    
+    func makeShoppingList(ingredients: [String], quantityOfItems: [Int])-> [String]
+    {
+        var newArray: [String] = []
+//        print(newArray)
+        for (index, item) in ingredients.enumerate()
+        {
+            var newString = ("\(index + 1). \(quantityOfItems[index]) \(item)")
+//            print(newString)
+            newArray.append(newString)
+//            print(newArray)
+        }
+//        print(newArray)
+        return newArray
     }
     
     
